b11c30a84fea0dcebdf1222bbe7756014884667c
Sarah Johnson <sarah.johnson@gsscogs.uk>
2024-01-25 08:10:38 +0000

    Setting up verified commits

--- dpytools/logger/logger.py
+++ dpytools/logger/logger.py
@@ -104,7 +104,7 @@ class DpLogger:
 
         event: the thing that's happened, a simple short english statement
         raw  : a raw string of any log messages captured for a third party library
-        data : arbitrary key values pairs that may be of use in providing context
+        data : arbitrary key-value pairs that may be of use in providing context
         """
         self._log(event, 10, raw=raw, data=data)
 
@@ -113,8 +113,8 @@ class DpLogger:
         Log at the info level.
 
         event: the thing that's happened, a simple short english statement
-        raw  : a raw string of any log messages captured for a third paty library
-        data : arbitrary key values pairs that may be of use in providing context
+        raw  : a raw string of any log messages captured for a third party library
+        data : arbitrary key-value pairs that may be of use in providing context
         """
         self._log(event, 20, raw=raw, data=data)
 
@@ -123,8 +123,8 @@ class DpLogger:
         Log at the warning level.
 
         event: the thing that's happened, a simple short english statement
-        raw  : a raw string of any log messages captured for a third paty library
-        data : arbitrary key values pairs that may be of use in providing context
+        raw  : a raw string of any log messages captured for a third party library
+        data : arbitrary key-value pairs that may be of use in providing context
         """
         self._log(event, 30, raw=raw, data=data)
 
@@ -134,8 +134,8 @@ class DpLogger:
 
         event: the thing that's happened, a simple short english statement
         error: a caught python Exceotion
-        raw  : a raw string of any log messages captured for a third paty library
-        data : arbitrary key values pairs that may be of use in providing context
+        raw  : a raw string of any log messages captured for a third party library
+        data : arbitrary key-value pairs that may be of use in providing context
         """
         self._log(event, 40, error=error, raw=raw, data=data)
 
@@ -151,7 +151,7 @@ class DpLogger:
 
         event: the thing that's happened, a simple short english statement
         error: a caught python Exceotion
-        raw  : a raw string of any log messages captured for a third paty library
-        data : arbitrary key values pairs that may be of use in providing context
+        raw  : a raw string of any log messages captured for a third party library
+        data : arbitrary key-value pairs that may be of use in providing context
         """
         self._log(event, 50, error=error, raw=raw, data=data)
